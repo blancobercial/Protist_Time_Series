ec5f7ad0d2fe7f23efee5218b0eca07ba7f54d6e
Leocadio Blanco-Bercial <48219176+blancobercial@users.noreply.github.com>
2021-11-17 16:05:34 -0400

    Update mefit.sh

--- mefit/mefit.sh
+++ mefit/mefit.sh
@@ -2,4 +2,6 @@
 
 mkdir mefit_results
 
+#make one line for each of the file pairs in the folder - example below
+
 ./mefit -s ./mefit_results/NS_30401 -r1 FILE_R1.fastq  -r2 FILE_R2.fastq  -p casper.params -meep 0.1
